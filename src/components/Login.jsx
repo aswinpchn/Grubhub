d51ef04dedda1fab1a5f0e3213f1b3a1af445b88
Aswin <aswin-pc@Aswins-MacBook-Air.local>
2019-09-22 15:49:01 -0700

    Added HandleLoginClick in login page.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,55 @@
 import React from 'react';
 import Header from './Header';
 import { Button, Form } from 'react-bootstrap';
+import axios from 'axios';
+import { exportDefaultSpecifier } from '@babel/types';
 class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loadLoginFor: null
+            loadLoginFor: null,
+            email: null,
+            password: null,
         }
         this.renderLoginForm = this.renderLoginForm.bind(this);
         this.renderCommonLoginButtons = this.renderCommonLoginButtons.bind(this);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.emailChangeHandler = this.emailChangeHandler.bind(this);
+        this.passwordChangeHandler = this.passwordChangeHandler.bind(this);
+    }
+
+    handleLoginClick(event) {
+        //prevent page from refresh
+        event.preventDefault();
+
+        axios.defaults.withCredentials = true;
+
+        axios.post('http://localhost:3001/user/login', {email : this.state.email, password : this.state.password})
+        .then(response => {
+            if(response.status == 200) {
+                console.log(response.data);
+            }   
+        }).catch((error) => {
+            if(error.response.status == 404) {
+                console.log('user not found');
+            } else if(error.response.status == 401) {
+                console.log('invalid password');
+            } else {
+                console.log('db error');
+            }
+        });    
+    }
+
+    emailChangeHandler = (e) => {
+        this.setState({
+            email : e.target.value
+        })
+    }
+
+    passwordChangeHandler = (e) => {
+        this.setState({
+            password : e.target.value
+        })
     }
 
     handleButtonClick(event) {
@@ -35,20 +76,20 @@ class Login extends React.Component {
                     You will be logged in as {this.state.loadLoginFor}
                 </Form.Text>
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control type="email" placeholder="Enter email" onChange={this.emailChangeHandler.bind(this)} value={this.state.email} required />
                     <Form.Control.Feedback type="invalid">
                         Please enter your username
                     </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control type="password" placeholder="Password" onChange={this.passwordChangeHandler.bind(this)} value={this.state.password} required />
                     <Form.Control.Feedback type="invalid">
                         Please enter your username
                     </Form.Control.Feedback>
                 </Form.Group>
                 <Button variant="link">Create new account</Button>
                 <div className="form-buttons">
-                    <Button className="form-login-buttons" variant="info" type="submit">Login</Button>
+                    <Button className="form-login-buttons" variant="info" type="submit" onClick={this.handleLoginClick.bind(this)}>Login</Button>
                     <Button className="form-login-buttons" variant="dark">Cancel</Button>
                 </div>
             </Form>
