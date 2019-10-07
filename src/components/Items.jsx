7204124acb49360a4becc56ff6a7b17bb58f30ff
Aswin <aswin-pc@MacBook-Pro.local>
2019-10-06 17:05:56 -0700

    Items - Added order button for non-empty order, remove key if count is zero in selectedItems list

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Alert, Form } from 'react-bootstrap';
+import { Card, Alert, Form, Button } from 'react-bootstrap';
 import { cloneDeep, groupBy, map } from 'lodash';
 
 class Items extends React.Component {
@@ -36,6 +36,9 @@ class Items extends React.Component {
         const itemCount = selectedItems[id] || 0;
         if(itemCount > 0) {
             selectedItems[id] = itemCount - 1;
+            if(selectedItems[id] === 0) {
+                delete selectedItems[id];
+            }
             this.setState({
                 selectedItems: selectedItems
             });
@@ -46,8 +49,8 @@ class Items extends React.Component {
         return(
             <Card className="shadow bg-white rounded" key={item.id}>
                 <Card.Body className="item-card">
-                    <Card.Title className="item-name">{item.name}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted item-details">{item.price} <span className="order-content"><i className="material-icons" onClick={() => this.decrementItem(item.id)}>remove_circle_outline</i><span className="quantity">{this.state.selectedItems[item.id] || '0'}</span><i className="material-icons" onClick={() => this.incrementItem(item.id)}>add_circle_outline</i></span></Card.Subtitle>
+                    <Card.Title className="item-name">{item.name}({item.description})</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted item-details">Price(Each):{item.price} <span className="order-content"><i className="material-icons" onClick={() => this.decrementItem(item.id)}>remove_circle_outline</i><span className="quantity">{this.state.selectedItems[item.id] || '0'}</span><i className="material-icons" onClick={() => this.incrementItem(item.id)}>add_circle_outline</i></span></Card.Subtitle>
                 </Card.Body>
             </Card>
         )
@@ -57,8 +60,11 @@ class Items extends React.Component {
         const items = cloneDeep(this.props.items);
         const groupedItems = groupBy(items, 'category');
         return(
-            <div className="item-list">
-                {map(groupedItems, this.renderGroup.bind(this))}
+            <div>
+                <div className="item-list">
+                    {map(groupedItems, this.renderGroup.bind(this))}
+                </div>
+                { this.state.selectedItems && Object.keys(this.state.selectedItems).length ?<Button>Order</Button> : "" }
             </div>
         );
     }
