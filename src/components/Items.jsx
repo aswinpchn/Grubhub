e39f3baf37599fe9d64f00e2c4a6d9cc59ee16a2
Aswin <aswin-pc@MacBook-Pro.local>
2019-10-08 00:07:17 -0700

    Items - Cart display while ordering.

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -9,11 +9,13 @@ class Items extends React.Component {
         super(props);
         this.state = {
             selectedItems: {},
+            basePrice : 0,
         }
 
         this.incrementItem = this.incrementItem.bind(this);
         this.decrementItem = this.decrementItem.bind(this);
         this.handleOrderClick = this.handleOrderClick.bind(this);
+        this.renderCart = this.renderCart.bind(this);
     }
 
     componentDidUpdate(prevProps) {
@@ -21,6 +23,7 @@ class Items extends React.Component {
             if(this.props.order.orderPlacingStatus === true)
                 this.setState({
                     selectedItems: {},
+                    cost : 0,
                 });
         }
     }
@@ -34,16 +37,17 @@ class Items extends React.Component {
         )
     }
 
-    incrementItem(id) {
+    incrementItem(id, price) {
         const selectedItems = cloneDeep(this.state.selectedItems);
         const itemCount = selectedItems[id] || 0;
         selectedItems[id] = itemCount + 1;
         this.setState({
-            selectedItems: selectedItems
+            selectedItems: selectedItems,
+            basePrice : this.state.basePrice + price,
         });
     }
 
-    decrementItem(id) {
+    decrementItem(id, price) {
         const selectedItems = cloneDeep(this.state.selectedItems);
         const itemCount = selectedItems[id] || 0;
         if(itemCount > 0) {
@@ -52,7 +56,8 @@ class Items extends React.Component {
                 delete selectedItems[id];
             }
             this.setState({
-                selectedItems: selectedItems
+                selectedItems: selectedItems,
+                basePrice : this.state.basePrice - price,
             });
         }
     }
@@ -62,7 +67,7 @@ class Items extends React.Component {
             <Card className="shadow bg-white rounded" key={item.id}>
                 <Card.Body className="item-card">
                     <Card.Title className="item-name">{item.name}({item.description})</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted item-details">Price(Each):{item.price} <span className="order-content"><i className="material-icons" onClick={() => this.decrementItem(item.id)}>remove_circle_outline</i><span className="quantity">{this.state.selectedItems[item.id] || '0'}</span><i className="material-icons" onClick={() => this.incrementItem(item.id)}>add_circle_outline</i></span></Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted item-details">Price(Each):{item.price} <span className="order-content"><i className="material-icons" onClick={() => this.decrementItem(item.id, item.price)}>remove_circle_outline</i><span className="quantity">{this.state.selectedItems[item.id] || '0'}</span><i className="material-icons" onClick={() => this.incrementItem(item.id, item.price)}>add_circle_outline</i></span></Card.Subtitle>
                 </Card.Body>
             </Card>
         )
@@ -85,6 +90,16 @@ class Items extends React.Component {
         this.props.createOrderTrigger(orderObject);
     }
 
+    renderCart() {
+        return (
+            <div>
+                <div>Cost before tax:{this.state.basePrice}</div>
+                <div>Tax : {this.state.basePrice*0.0925}</div>
+                <div>Total cost : {this.state.basePrice*0.0925 + this.state.basePrice}</div>
+            </div>
+        );
+    }
+
     render() {
         const items = cloneDeep(this.props.items);
         const groupedItems = groupBy(items, 'category');
@@ -93,6 +108,7 @@ class Items extends React.Component {
                 <div className="item-list">
                     {map(groupedItems, this.renderGroup.bind(this))}
                 </div>
+                {this.state.selectedItems && Object.keys(this.state.selectedItems).length !== 0 ? (this.props.order.orderPlacingStatus === true ? "" :  this.renderCart()) : "" }
                 {this.state.selectedItems && Object.keys(this.state.selectedItems).length !== 0 ? (this.props.order.orderPlacingStatus === true ? "" : <Button onClick={this.handleOrderClick}>Order</Button> ) : ""}
             </div>
         );
